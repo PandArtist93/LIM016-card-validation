5a82fa0c185bc7e23d2b7970deb2af3312c2b119
Marielena <maizaga01@gmail.com>
2021-10-18 10:09:26 -0500

    se añadió método isValid y maskify, regex en los inputs

--- src/index.js
+++ src/index.js
@@ -1,80 +1,96 @@
-/* import { clean } from 'gh-pages';
+//import { clean } from 'gh-pages';
 import validator from './validator.js';
 
-console.log(validator); */
+//console.log(validator);
 
 /*---------función que se encarga de limpiar los campos del formulario------------*/
 
 function cleanFormFields() {
     alert("¿estas seguro de que deseas borrar todo?");
-};
+        
+}
 
-let bntClean = document.getElementById("clean");
-bntClean.addEventListener("click", cleanFormFields);
+let cleanFieldsBnt = document.getElementById("clean");
+cleanFieldsBnt.addEventListener("click", cleanFormFields);
 
 /*---------función que se encarga de actualizar los datos de la tarjeta------------*/
 
 function updateCardName(event){
+    event.target.value = event.target.value.replace(/\d/g,'');
     let nameValue = event.target.value; 
-
     let cardName = document.getElementById("nameCard");
-
     cardName.innerHTML = nameValue;
-};
+
+}
 
 let inputCardName = document.getElementById("nameForm");
 inputCardName.addEventListener("input", updateCardName);
 
 
 function updateCardNumber(event){
+    event.target.value = event.target.value.replace(/\D/g,'');
     let numberValue = event.target.value;
+    let cardNumber = document.getElementById("numberCard");    
+    cardNumber.innerHTML = validator.maskify(numberValue);
 
-    let cardNumber = document.getElementById("numberCard");
-    
-    cardNumber.innerHTML = numberValue;
-};
+}
 
 let inputCardNumber = document.getElementById("numbForm");
 inputCardNumber.addEventListener("input", updateCardNumber);
 
 function updateCardCvv(event){
-    let cvvValue = event.target.value;
-
+    event.target.value = event.target.value.replace(/\D/g,'');
+    let cvvValue = event.target.value;    
     let cardCVV = document.getElementById("cvvCard");
-
-    cardCVV.innerHTML = cvvValue;
-}; 
+    cardCVV.innerHTML = cvvValue;    
+}
 
 let inputCardCvv = document.getElementById("codeCvvForm");
 inputCardCvv.addEventListener("input", updateCardCvv);
 
 function updateCardMonth(event){
+    event.target.value = event.target.value.replace(/\D/g,'');
     let monthValue = event.target.value;
-
-    let cardMonth = document.getElementById("dateCardMonth");
-    
+    let cardMonth = document.getElementById("dateCardMonth");    
     cardMonth.innerHTML = monthValue;
-};
+}
 
 let inputCardMonth = document.getElementById("dateFormMonth");
 inputCardMonth.addEventListener("input", updateCardMonth);
 
 function updateCardYear(event){
+    event.target.value = event.target.value.replace(/\D/g,'');
     let yearValue = event.target.value;
-
-    let cardYear = document.getElementById("dateCardYear");
-    
+    let cardYear = document.getElementById("dateCardYear");    
     cardYear.innerHTML = yearValue;
-};
+}
 
 let inputCardYear = document.getElementById("dateFormYear");
 inputCardYear.addEventListener("input", updateCardYear);
 
-/* función que se encarga de validar la tarjeta*/
+function validaterCardNumb(event){
+    //console.log(event);
+    event.preventDefault();
+    
+    let cardNumb = document.getElementById("numbForm");
+    let cardNumbValue = cardNumb.value;
 
-/* function validateCard(){
+    if (cardNumbValue.length <= 16){
+        let result = validator.isValid(cardNumbValue);
+        if (result == true){
+            alert("tu tarjeta es válida!");
+        }
+        else {
+            alert("tu tarjeta no es válida, por favor ingresa la tarjeta correctamente");
+        }        
+    }
+    else{
+        alert("Debes ingresar un número de tarjeta válido\nEl número de la tarjeta es valido si posee máximo 16 dígitos");
+    }
+}
 
-}; */
+let validateCardBtn = document.getElementById("validateBtn");
+validateCardBtn.addEventListener("click", validaterCardNumb);
 
 
 
--- src/validator.js
+++ src/validator.js
@@ -1,6 +1,74 @@
 const validator = {
-  // ...
+  isValid: function(creditCardNumber){
+    console.log(creditCardNumber);
+    let validationDigit = creditCardNumber[creditCardNumber.length -1]
+    let realCreditCardNumber = creditCardNumber.substr(0, creditCardNumber.length - 1);
+    console.log(realCreditCardNumber);
+    let cardNumbInt = newArrayNumb(realCreditCardNumber);
+    for (let i = 0; i < cardNumbInt.length; i = i+2){
+      cardNumbInt[i] = cardNumbInt[i] * 2;
+      if(cardNumbInt[i] > 9){
+        let lastDigit = cardNumbInt[i] % 10;
+        let firstDigit = Math.floor(cardNumbInt[i] / 10);
+        cardNumbInt[i] = firstDigit + lastDigit;
+      }      
+    }
+    let sumTotal = sumNumbArray(cardNumbInt);
+    console.log(sumTotal);
+
+    /* if (sumTotal != 0 && sumTotal % 10 == 0){
+      return true
+    }
+    else{
+      return false
+    } */
+
+    if (sumTotal != 0){
+      if (validationDigit == 10 - (sumTotal % 10)){
+        
+        return true
+      }
+      return false   
+    }
+    return false  
+  },
+
+  maskify: function(creditCardNumber){
+    let numbString = creditCardNumber;
+    if(numbString.length < 5){
+      return numbString
+    }
+    
+    let newArrayNumbStrig = "";
+    for (let i = 0; i < numbString.length; i++){
+      if (i < numbString.length-4){
+        newArrayNumbStrig += "#";
+      }
+      else {
+        newArrayNumbStrig += numbString[i];
+      }      
+    }  
+    return newArrayNumbStrig
+  }
 };
+
+
+function newArrayNumb(stringCardNumb){
+  
+  let numbArray = [];
+  for (let i = stringCardNumb.length -1 ; i > -1 ; i--){
+      numbArray.push(parseInt(stringCardNumb[i]));      
+  } 
+  return numbArray 
+}
+
+function sumNumbArray (numbArray){
+  let sumNumb = 0;
+  for (let i = 0; i < numbArray.length; i++){
+    sumNumb = sumNumb + numbArray[i];
+  }  
+  return sumNumb
+}
 
 export default validator;
 
